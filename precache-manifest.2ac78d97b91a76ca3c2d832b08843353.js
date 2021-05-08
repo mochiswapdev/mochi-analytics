873c907e7e9e4012e4fa617dae859ed3a8147102
frankydev1 <72719344@users.noreply.github.com>
2021-05-08 14:08:08 -0400

    updates

---
+++ precache-manifest.2ac78d97b91a76ca3c2d832b08843353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfd664a714a7c4690cdc47157eb8e54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fb364927f7682584448",
+    "url": "/static/js/2.bede49dd.chunk.js"
+  },
+  {
+    "revision": "765dfa9de4a132345b022dd1d11a87d0",
+    "url": "/static/js/2.bede49dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01dede3f6c1ec63fd0d6",
+    "url": "/static/js/main.4ebe0c18.chunk.js"
+  },
+  {
+    "revision": "b70e09398a22d998e042",
+    "url": "/static/js/runtime-main.f63dbb3b.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "2f7ca5f5e57aa35566b3aae9d7d28e10",
+    "url": "/static/media/logo.2f7ca5f5.png"
+  }
+]);
